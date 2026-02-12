58fe2730850770f27f84d41065c6f010f36de47b
GABS2503 <gabrielltaroreh6@gmail.com>
2026-02-13 00:12:01 +0800

    Initial commit

---
+++ src/api/livestream-comment/controllers/livestream-comment.ts
@@ -0,0 +1,7 @@
+/**
+ * livestream-comment controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::livestream-comment.livestream-comment');
---
+++ src/api/livestream-comment/routes/livestream-comment.ts
@@ -0,0 +1,7 @@
+/**
+ * livestream-comment router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::livestream-comment.livestream-comment');
