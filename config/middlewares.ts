157e9962e2cef826dea2fadb908a53fe9435b29b
GABS2503 <100610078+GABS2503@users.noreply.github.com>
2026-02-18 23:03:09 +0800

    Refactor CORS middleware configuration
    Updated the CORS middleware configuration to enable it and specify allowed origins and methods.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,22 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:1337',
+        // This * allows ALL websites. It is the easiest fix for now.
+        '*', 
+        // OR you can be specific (optional):
+        // 'https://web-skripsi-7cggkx78i-gabs2503s-projects.vercel.app', 
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
